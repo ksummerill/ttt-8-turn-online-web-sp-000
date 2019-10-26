dc4609a63c319f7e79680881ae02a28f20f65011
Kate Summerill <kaitlynsummerill@gmail.com>
2019-10-26 05:13:39 +0000

    Automatically backed up by Learn

---
+++ lib/turn.rb
@@ -0,0 +1,57 @@
+# display empty board
+def display_board(board)
+  puts " #{board[0]} | #{board[1]} | #{board[2]} "
+  puts "-----------"
+  puts " #{board[3]} | #{board[4]} | #{board[5]} "
+  puts "-----------"
+  puts " #{board[6]} | #{board[7]} | #{board[8]} "
+end
+
+# code your input_to_index
+  def input_to_index(input)
+    # convert input to integer and subtracts 1
+    input.to_i - 1
+  end
+
+# code the move method here!
+  def move(array, index, character = "X")
+    array[index] = character
+  end
+
+  # code your #position_taken? method here!
+
+  def position_taken?(array, index)
+    if array[index] == " " || array[index] == "" || array[index] == nil
+      return false
+    elsif array[index] == "X" || array[index] == "O"
+      return true
+    end
+  end
+
+# code your #valid_move? method here
+def valid_move?(array, index)
+  if index.between?(0,8) && !position_taken?(array, index)
+    return true
+  else position_taken?(array, index) == true
+    return false
+  end
+end
+
+# turn method
+# ask for user input, gets user input
+def turn(array)
+  puts "Please enter 1-9:"
+  input = gets.strip
+  input_to_index(input)
+  # valid_move?(array, index)
+end
+
+
+
+
+# if index is valid
+#   make the move for index
+#   show the board
+# else
+#   ask for input again until you get a valid input
+# end
